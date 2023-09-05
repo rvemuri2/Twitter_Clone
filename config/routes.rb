8b7e784fa6f39f90ec2efe42efacc90243c46868
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-05 10:43:30 -0700

    Made login form for users

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
 
   post "sign_up", to: "registrations#create"
 
+  get "sign_in", to: "sessions#new"
+  post "sign_in", to: "sessions#create" 
+
   delete "logout", to: "sessions#destroy"
 
   root to: "main#index"

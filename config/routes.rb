4e296f2353e822b98c911ee6c8366133e4367be6
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-04 22:38:23 -0700

    Created a sign-up form using bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,10 @@ Rails.application.routes.draw do
   # GET /about
   get "about-us", to: "about#index", as: :about
 
+  get "sign_up", to: "registrations#new"
+
+  post "sign_up", to: "registrations#create"
+
   root to: "main#index"
   
 

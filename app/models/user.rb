9123c60d067879233da165ce5a3fa34895ec0174
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-04 22:19:49 -0700

    Created validations for creating a user

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,6 @@
 class User < ApplicationRecord
 
     has_secure_password
+
+    validates :email, presence:true, format: { with: /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i, message: "must be a valid email address"}
 end

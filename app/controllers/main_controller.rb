24061c05929b11c5ae89af02338d082a321623c0
Rohit Vemuri <rohitvemuri@yahoo.com>
2023-09-04 23:51:42 -0700

    logging in with session cookies

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,6 +1,9 @@
 class MainController < ApplicationController
     def index
-        flash.now[:notice] = "Logged in successfully"
-        flash.now[:alert] = "Invalid email or password"
+        
+        if session[:user_id] 
+            @user = User.find_by(id: session[:user_id])
+            flash[:message] = "Welcome back", @user
+        end
     end
 end
